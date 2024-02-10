f2f6c470585a69d7ea1df237e0cf84101ee22348
tristan <89363868+who4reu@users.noreply.github.com>
2024-02-10 21:34:41 +0700

    Create Settings.lua

---
+++ StarterGui/MainScreen/MainFrame/Settings.lua
@@ -0,0 +1,5 @@
+local NewBlur = Instance.new('BlurEffect', game.Lighting)
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+NewBlur.Size = 25 
